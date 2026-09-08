5f482dba2b58921f8aecaf124816a3438b82dadc
adsdigitall <adsdigital47@gmail.com>
2026-09-08 02:35:16 -0300

    chore: use official eslint quality gate rules verbatim

--- eslint-rules/index.cjs
+++ eslint-rules/index.cjs
@@ -1,3 +1,16 @@
 "use strict";
-const { maxLines, noDirectConsole, noDirectDataAccess } = require("./core-rules.cjs");
-module.exports = { rules: { "max-lines": maxLines, "no-direct-console": noDirectConsole, "no-direct-data-access": noDirectDataAccess } };
+
+const {
+  maxLines,
+  noDirectConsole,
+  noDirectDataAccess,
+} = require("./core-rules.cjs");
+
+module.exports = {
+  rules: {
+    "max-lines": maxLines,
+    "no-direct-console": noDirectConsole,
+    "no-direct-data-access": noDirectDataAccess,
+  },
+};
+
